55a1db2237297b8e707c38c3506f75e469d2b6c4
João Bispo <vitor.vi10@hotmail.com>
2021-08-21 15:30:21 -0300

    feat: add FormMenu structure

--- src/components/FormMenu/index.tsx
+++ src/components/FormMenu/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Navigation, DropboxIcon, Form } from './styles';
 
 const FormMenu: React.FC = () => {
-  return <Container>abccc</Container>;
+  return (
+    <Container>
+      <Navigation>
+        <h1>
+          <DropboxIcon />
+          <span>Dropbox</span>
+        </h1>
+        <button className="action--close">✕</button>
+      </Navigation>
+
+      <Form>
+        <b className="title">Registre-se</b>
+        <span className="subtitle">Preencha o formulário abaixo</span>
+
+        <input type="text" placeholder="Nome" />
+        <input type="text" placeholder="Sobrenome" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+
+        <button type="submit">Prosseguir</button>
+
+        <span className="terms">
+          Esta página está sujeita à Política de privacidade e aos Termos de
+          serviço.
+        </span>
+      </Form>
+    </Container>
+  );
 };
 
 export default FormMenu;
--- src/components/FormMenu/styles.ts
+++ src/components/FormMenu/styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components';
 
-export const Container = styled.form``;
+export const Container = styled.div``;
+
+export const Navigation = styled.header``;
+export const DropboxIcon = styled.div``;
+export const Form = styled.form``;
