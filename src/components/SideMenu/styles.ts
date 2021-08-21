0db0453e406880c273e43b69ae729c47f35800c2
João Bispo <vitor.vi10@hotmail.com>
2021-08-21 15:10:20 -0300

    feat: add side menu styles

---
+++ src/components/SideMenu/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+  z-index: 10;
+  position: fixed;
+
+  top: 0;
+  right: 0;
+
+  width: 100%;
+  height: 100%;
+  background: var(--tertiary);
+
+  transform: translateX(100%);
+
+  transition: transform 0.5s cubic-bezier(0.5, 0, 0, 1);
+
+  &.open {
+    transform: translateX(0);
+  }
+
+  @media (min-width: 1024px) {
+    width: calc(100% / 3);
+    box-shadow: -7px 0 5px -6px rgba(0, 0, 0, 0.075);
+
+    &.scrollOpen {
+      transform: translateX(0);
+    }
+
+    &.scrollOpen .action--close {
+      display: none;
+    }
+  }
+
+  @media (min-width: 1440px) {
+    width: calc(480px + ((100vw - 1440px) / 2));
+  }
+`;
