37b47dd8f2bcfafaab6a1a9490e9cddc7161fa66
João Bispo <joaobispo2077@gmail.com>
2021-08-21 11:12:11 -0300

    feat: add sections and contents

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -41,8 +41,27 @@ export const Container = styled.section`
       --heading-font-size: 71px;
     }
   }
+
+  background-color: var(--container-background);
 `;
 
 export const Content = styled.div`
   z-index: 2;
+  position: relative;
+  max-width: 1440px;
+  margin: 0 auto;
+
+  > h2 {
+    font-size: var(--heading-font-size);
+    color: var(--logo-color);
+  }
+
+  > p {
+    margin-top: 20px;
+    font-size: 16px;
+    color: var(--text-color);
+    max-width: var(--content-width);
+  }
+
+  padding: var(--padding-top) 32px var(--padding-bottom);
 `;
--- src/data/index.ts
+++ src/data/index.ts
@@ -6,7 +6,7 @@ export default [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis. Donec non ullamcorper sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
   },
   {
-    variant: 'blue',
+    variant: 'beige',
     title: 'Diga adeus ao trabalho maçante',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis. Donec non ullamcorper sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
@@ -18,12 +18,12 @@ export default [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis. Quisque et elit ut eros ultrices mollis.',
   },
   {
-    variant: 'blue',
+    variant: 'white',
     title: 'Para todos',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   },
   {
-    variant: 'blue',
+    variant: 'black',
     title: 'Parte inferior da página',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi sit amet nisi volutpat facilisis.',
